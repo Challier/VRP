2a05604dfa53dd962f82c26067739d82e54c2c28
Challier <challier.victor@gmail.com>
2017-10-08 00:52:53 +0200

    remove ugly ass tabs

--- src/App.vue
+++ src/App.vue
@@ -43,34 +43,6 @@
         <img src="https://i.imgur.com/TuH1uGz.jpg" alt="avatar">
       </v-avatar>
     </v-toolbar>
-      <v-tabs dark fixed icons centered>
-      <v-tabs-bar class="cyan">
-        <v-tabs-slider class="yellow"></v-tabs-slider>
-        <v-tabs-item href="#tab-1">
-          <v-icon>phone</v-icon>
-          Recents
-        </v-tabs-item>
-        <v-tabs-item href="#tab-2">
-          <v-icon>favorite</v-icon>
-          Favorites
-        </v-tabs-item>
-        <v-tabs-item href="#tab-3">
-          <v-icon>account_box</v-icon>
-          Nearby
-        </v-tabs-item>
-      </v-tabs-bar>
-      <v-tabs-items>
-        <v-tabs-content
-          v-for="i in 3"
-          :key="i"
-          :id="'tab-' + i"
-        >
-          <v-card flat>
-            <v-card-text>{{ text }}</v-card-text>
-          </v-card>
-        </v-tabs-content>
-      </v-tabs-items>
-    </v-tabs>
     <main>
       <v-content>
         <v-container fluid>
